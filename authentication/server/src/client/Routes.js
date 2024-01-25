638453aeb01f14d2868ad22b36bde4b23447bbc1
Vladimir Stoyanov <vladsto101@gmail.com>
2024-01-26 00:38:47 +0200

    Added authentication and styling.

---
+++ authentication/server/src/client/Routes.js
@@ -0,0 +1,20 @@
+import App from './App';
+import HomePage from './pages/HomePage';
+import UsersListPage from './pages/UsersListPage';
+
+export default [
+    {
+        ...App,
+        routes: [
+            {
+                ...HomePage,
+                path: '/',
+                exact: true
+            },
+            {
+                ...UsersListPage,
+                path: '/users',
+            }
+        ]
+    }
+];
